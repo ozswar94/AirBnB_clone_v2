0624513652b42a0c841440d445127ba1342c3e1e
ozswar94 <3630@holbertonschool.com>
2022-04-14 10:40:04 +0000

    add task 8

--- models/city.py
+++ models/city.py
@@ -23,7 +23,10 @@ class City(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", cascade='all, delete, delete-orphan',
+                              backref="cities")
 
     else:
         state_id = ""
         name = ""
+        places = ""
--- models/place.py
+++ models/place.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ class place definition """
 from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ class Place that inherits from Base
         Attribute:
             city_id: City.id()
@@ -18,17 +22,15 @@ class Place(BaseModel):
             longitude: longitude (float)
             amenity_ids: Amenity.id()
             """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
