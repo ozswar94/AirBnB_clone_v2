d1a8bf0910eceb3ed2b30723b11811953dab3855
Ozswar <jordan6heat@hotmail.fr>
2022-04-28 10:24:11 +0000

    -0: start -> restart ; change name of config file| -3 fix error pycodestyle

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Hello World" |sudo tee  /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown ubuntu:ubuntu  -hR /data/
-sudo sed -i '47i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo sed -i '47i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
