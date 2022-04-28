282168671e4dde6fdd2854a4d459b583fcf7a9e0
Ozswar <jordan6heat@hotmail.fr>
2022-04-28 08:20:43 +0000

    add sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # Task 0 of deploiment
 sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo "Hello World" > /data/web_static/releases/test/index.html
+echo "Hello World" |sudo tee  /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown ubuntu:ubuntu  -hR /data/
 sudo sed -i '47i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
