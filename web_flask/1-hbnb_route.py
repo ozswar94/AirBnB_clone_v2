4971a05162f65a62e29d44570f370d1f5252ede2
ozswar94 <3630@holbertonschool.com>
2022-05-14 09:33:17 +0000

    fix pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+""" print HBNB """
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -15,4 +15,5 @@ def print_hbnb():
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
