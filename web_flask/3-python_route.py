ee851694080f641f17274df77357f5fd3be852ce
ozswar94 <3630@holbertonschool.com>
2022-05-14 09:44:24 +0000

    pep8 fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+""" print Python and variable with multi route """
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -30,4 +30,5 @@ def print_python(text=None):
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
